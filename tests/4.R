df5fd21ae34cdb0b4e1e3ea0f186b46016f52a4a
Joshua Marsh <2155744M@student.gla.ac.uk>
2016-08-05 11:40:20 +0100

    Added a CreateCustomTimeSeries function to make it easier for users to make template patterns. Also added some stuff to vignette.

---
+++ tests/4.R
@@ -0,0 +1,38 @@
+#'Test Case for create_time_series.R
+#'@import RUnit, xts
+library(RUnit)
+
+test.CreateCustomTimeSeries <- function() {
+  ts = CreateCustomTimeSeries(
+    c(1,2,3,4,5),
+    c(1,2,3,4)
+  )
+
+#Check the object creates is xts
+checkTrue(xts::is.xts(ts))
+
+#Check one of the values and intervals in the time series to ensure they are correct.
+#Also check it is the correct length.
+checkTrue(ts[[2]]==2)
+
+checkTrue(time(ts[2]) - time(ts[1]) == 1)
+
+checkTrue(length(ts)==5)
+
+#Check that exceptions are thrown when length(intervals) != length(values)-1
+checkException(CreateCustomTimeSeries(
+  c(1,2,3,4,5),
+  c(1,2,3)
+))
+
+checkException(CreateCustomTimeSeries(
+  c(1,2,3,4,5),
+  c(1,2,3,4,5)
+))
+
+checkException(CreateCustomTimeSeries(
+  c(1,2,3,4,5),
+  c(1,2,3,4,5,6)
+))
+
+}
